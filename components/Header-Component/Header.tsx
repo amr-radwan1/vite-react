dba77be5a70bbce690f544a73a7aa085701ddab0
Kostika Nini <ninik@sheridancollege.ca~>
2024-10-21 23:01:45 -0400

    commiting

--- components/Header-Component/Header.tsx
+++ components/Header-Component/Header.tsx
@@ -1,10 +1,29 @@
 import './Header.css'
 import Button from "@mui/material/Button";
+import MenuIcon from '@mui/icons-material/Menu'
+import IconButton from '@mui/material/IconButton';
 import Logo from '/logo.png'
 function Header(){
     return(
         <div className="header">
-             <img src={Logo} />
+             <img src={Logo}/>
+             <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{
+                    mr: 2,
+                    display: 'none',
+                    '@media (max-width:768px)': {
+                    display: 'block',
+                    position: 'absolute',
+                    top:'0px',
+                    },
+                }}
+>
+  <MenuIcon />
+</IconButton>
 
              <div className="header-button">
                 <a href="#AboutUs">
@@ -16,6 +35,7 @@ function Header(){
                 
                 <Button variant="outlined" color="primary">GALLERY</Button> 
                 <Button variant="outlined" color="primary" id="contact-btn">Contact</Button> 
+                
              </div>
         </div>
        
